8e422ebdf1cd83bdf9316985f9dd6c9e6e4f2474
Emanuel K <ekrivoy@gmail.com>
2016-01-06 22:35:42 -0430

    Created Utils
    Moved misc. methods that were in Controller to Utils

--- bin/CA.js
+++ bin/CA.js
@@ -865,3 +865,63 @@ function Format() {
 		return matrix;
 	} 
 }
+var CA = CA || {};
+CA.Utils = {};
+
+
+/**
+*Runs the model indefinitely, updating and painting it every updateEvery milis
+*/
+CA.Utils.stepEvery = function(updateEvery, model, painter) {
+	painter.paintGrid(model.matrix);
+
+	var intervalID = setInterval(function() {
+
+		var changedCells  = model.step();
+		painter.paintCells(changedCells);
+
+	}, updateEvery);
+
+}
+
+/**
+*Runs the model for n steps, updating and painting it every updateEvery milis
+*/
+CA.Utils.stepFor = function(n, updateEvery, painter, model) {
+	painter.paintGrid(model.matrix);
+
+	var intervalID = setInterval(function() {
+
+		var changedCells  = model.step();
+		painter.paintCells(changedCells);
+
+		if(n--<0) {
+			clearInterval(intervalID);
+		}
+
+	}, updateEvery);
+}
+
+/**
+*Returns a new model with a matrixWidth and matrixheight that completely fills the window.
+*CellLength specifies the side length of painted cells
+*width and height specify the dimensions of the element where the model will be painted
+*/
+CA.Utils.getModelFitToDimensions = function(cellLength, width, height) {
+	var xCells = Math.floor(width/cellLength);
+	var yCells = Math.floor(height/cellLength);
+
+	return new CA.Model(xCells, yCells);
+}
+/**
+*Returns the maximum bewtween the cellLength thats fits the model to the window and 1.
+*/
+CA.Utils.getCellLengthToFitModel = function(model, width, height) {
+	var width = Math.floor(window.innerWidth/model.matrixWidth);
+	var height = Math.floor(window.innerHeight/model.matrixHeight);
+
+	width = width > 0? width:1;
+	height = height > 0? height:1;
+
+	return Math.min(width,height);
+}
--- controller.js
+++ controller.js
@@ -6,15 +6,7 @@ var painter;
 */
 function setUp() {
 
-	//Model Set Up
-	model = getModelFitToScreen(3);
-
-	var prefabs = new CA.Prefabs(model);
-
-	prefabs.transersII()
-
-	//Painter Set Up
-	var colors = prefabs.transersII.getColors();
+	var cellLength = 3;
 
 	var canvas = document.getElementById("canvas");
 
@@ -23,64 +15,19 @@ function setUp() {
 	$(canvas).attr("width", self.windowWidth);
 	$(canvas).attr("height", self.windowHeight);
 
-	painter = new CA.Painter(colors, getCellLengthToFitModel(model), "canvas");
+	//Model Set Up
+	model = CA.Utils.getModelFitToDimensions(cellLength, self.windowWidth, self.windowHeight);
+
+	var prefabs = new CA.Prefabs(model);
+
+	prefabs.transersII()
+
+	//Painter Set Up
+	var colors = prefabs.transersII.getColors();
+
+
+	painter = new CA.Painter(colors, cellLength, "canvas");
 
 	//Steps to the next generation every 10 ms
-	start(10);
-}
-
-/**
-*Runs the model indefinitely, updating and painting it every updateEvery milis
-*/
-function start(updateEvery) {
-	painter.paintGrid(model.matrix);
-
-	var intervalID = setInterval(function() {
-		
-		var changedCells  = model.step();
-		painter.paintCells(changedCells);
-
-	}, updateEvery);
-
-}
-
-/**
-*Runs the model for n steps, updating and painting it every updateEvery milis
-*/
-function runFor(n, updateEvery) {
-	painter.paintGrid(model.matrix);
-
-	var intervalID = setInterval(function() {
-		
-		var changedCells  = model.step();
-		painter.paintCells(changedCells);
-
-		if(n--<0) {
-			clearInterval(intervalID);
-		}
-
-	}, updateEvery);
-}
-
-/**
-*Returns a model with a matrixWidth and matrixheight that completely fills the window.
-*CellLength specifies the side length of painted cells
-*/
-function getModelFitToScreen(cellLength) {
-	var xCells = Math.floor(window.innerWidth/cellLength);
-	var yCells = Math.floor(window.innerHeight/cellLength);
-
-	return new CA.Model(xCells, yCells);
-}
-/**
-*Returns the maximum bewtween the cellLength thats fits the model to the window and 1.
-*/
-function getCellLengthToFitModel(model) {
-	var width = Math.floor(window.innerWidth/model.matrixWidth);
-	var height = Math.floor(window.innerHeight/model.matrixHeight);
-
-	width = width > 0? width:1;
-	height = height > 0? height:1;
-
-	return Math.min(width,height);
+	CA.Utils.stepEvery(10, model, painter);
 }
